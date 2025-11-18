39a2928cd691df492b37c6a36a2299c6e5c68ca1
Hkuser18 <hadarkeller@gmail.com>
2025-11-18 18:14:21 +0200

    added cool calculations

--- 04_HtmlElements/index.js
+++ 04_HtmlElements/index.js
@@ -1,5 +1,48 @@
+//04_HtmlElements/index.js
+document.addEventListener("DOMContentLoaded", () => {
+    pageLoaded();
+});
+
+
+let in1;
+let in2;
+let btnRes;
+let res;
+function pageLoaded() {
+    in1 = document.querySelector("#input1");
+    in2 = document.querySelector("#input2");
+    res = document.getElementById("lblRes");
+    btnRes = document.getElementById("btnRes");
+    btnRes.addEventListener("click", () => {
+        calculate();
+    });
+}
+
+const btn2 = document.getElementById("btn2");
+
+
+btn2.addEventListener("click", () => {
+    print("btn2 clicked" + btn2.id + "|" + btn2.innerText);
+});
+
+function calculate() {
+    let in1txt = in1.value;
+    let in2txt = in2.value;
+    let num1 = parseInt(in1txt);
+    let num2 = parseInt(in2txt);
+    let sum = num1 + num2;
+    res.innerText = sum;
+}
+
+/*btn2.addEventListener("click",func1());
+
+function func1(){   }*/
+
+
 function print(msg) {
+    //get text area element reference
     const ta = document.getElementById("output");
+    //--write to text area--
     if (ta) ta.value = msg;
     else console.log(msg);
 }
